298fd46fa285d82de76e7bc303de3555790d3943
pedroemanuel2701 <160974332+pedroemanuel2701@users.noreply.github.com>
2025-05-16 15:39:16 -0300

    Aplicação Java
    Subi o nosso backend Java bravíssimo
    
    Consiste nas dependências:
    Spring Web (spring-boot-starter-web)
    → Para criar endpoints REST.
    
    Spring Data JPA (spring-boot-starter-data-jpa)
    → Para banco de dados e repositórios.
    
    PostgreSQL Driver (ou H2 Database para testes)
    → Conexão com o banco.
    
    Lombok
    → Para reduzir código boilerplate (getters/setters).
    
    Validation (spring-boot-starter-validation)
    → Validação de campos (@NotBlank, @Future, etc.).

---
+++ consultóriodonto/consultóriodonto/src/test/java/com/consultoriodonto/consultoriodonto/ConsultoriodontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultoriodonto.consultoriodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
